ea47a22082b498d483263ea27e35fcbcd045d437
Yellowman <marc.inthaamnouay@gmail.com>
2018-12-17 16:17:36 +0100

    - Add airliner mutation

--- src/Validator/AbstractAircraftModel.php
+++ src/Validator/AbstractAircraftModel.php
@@ -37,4 +37,52 @@ abstract class AbstractAircraftModel
      * @Assert\NotNull
      */
     public $cargo;
+
+    /**
+     * @return int
+     */
+    public function getPassenger(): int
+    {
+        return $this->passenger;
+    }
+
+    /**
+     * @param int $passenger
+     */
+    public function setPassenger(int $passenger): void
+    {
+        $this->passenger = $passenger;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOwner(): string
+    {
+        return $this->owner;
+    }
+
+    /**
+     * @param string $owner
+     */
+    public function setOwner(string $owner): void
+    {
+        $this->owner = $owner;
+    }
+
+    /**
+     * @return float
+     */
+    public function getCargo(): float
+    {
+        return $this->cargo;
+    }
+
+    /**
+     * @param float $cargo
+     */
+    public function setCargo(float $cargo): void
+    {
+        $this->cargo = $cargo;
+    }
 }
